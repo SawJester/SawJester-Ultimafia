f102f067a5ec46f6e1d1f172fa641f91f786d295
SawJester <anglelord270@gmail.com>
2024-08-08 13:06:04 -0400

    Update Rotten.js

--- Games/types/Mafia/roles/cards/Rotten.js
+++ Games/types/Mafia/roles/cards/Rotten.js
@@ -1,13 +1,24 @@
 const Card = require("../../Card");
+const Random = require("../../../../../lib/Random");
+const { PRIORITY_NIGHT_ROLE_BLOCKER } = require("../../const/Priority");
 
-module.exports = class StartWithArmor extends Card {
+module.exports = class Rotten extends Card {
   constructor(role) {
     super(role);
 
-    this.hideModifier = {
-      self: true,
-    };
+    this.actions = [
+      {
+        priority: PRIORITY_NIGHT_ROLE_BLOCKER,
+        labels: ["block", "hidden", "absolute"],
+        run: function () {
+          if (this.game.getStateName() != "Night") return;
 
-    this.startItems = ["PermaMindRot"];
+          //if (!this.actor.alive) return;
+          
+            this.blockWithMindRot(this.actor);
+          
+        },
+      },
+    ];
   }
 };
