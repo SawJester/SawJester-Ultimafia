6525e68c77672252b24714e85a0aec30fa9ebf40
SawJester <anglelord270@gmail.com>
2024-08-08 12:57:20 -0400

    Create Rotten.js

---
+++ Games/types/Mafia/roles/cards/Rotten.js
@@ -0,0 +1,13 @@
+const Card = require("../../Card");
+
+module.exports = class StartWithArmor extends Card {
+  constructor(role) {
+    super(role);
+
+     this.hideModifier = {
+      self: true,
+    };
+
+    this.startItems = ["PermaMindRot"];
+  }
+};
