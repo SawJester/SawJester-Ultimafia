e1771a458529ee14dea6017d56083032bff4fe2c
SawJester <anglelord270@gmail.com>
2024-10-22 07:34:38 -0400

    Create VotingMadness.js

---
+++ Games/types/Mafia/effects/VotingMadness.js
@@ -0,0 +1,36 @@
+const Effect = require("../Effect");
+const Action = require("../Action");
+
+module.exports = class VotingMadness extends Effect {
+  constructor(curser, target, lifespan) {
+    super("VotingMadness");
+    this.curser = curser;
+    this.target = target;
+    this.lifespan = lifespan;
+
+    this.listeners = {
+      vote: function (vote) {
+        if (
+          vote.meeting.name === "Village" &&
+          vote.voter === this.player &&
+          vote.target === this.target.id
+        ) {
+          let action = new Action({
+            actor: this.curser,
+            target: this.player,
+            game: this.game,
+            labels: ["kill", "curse", "hidden"],
+            effect: this,
+            power: 2,
+            run: function () {
+              if (this.dominates()) this.target.kill("basic", this.actor, true);
+              this.effect.remove();
+            },
+          });
+
+          this.game.instantAction(action);
+        }
+      },
+    };
+  }
+};
