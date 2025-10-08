ea7281ba6b804004b3b81273946883a2792d6704
SawJester <anglelord270@gmail.com>
2025-10-08 17:52:06 -0400

    Update Scrambled.js

--- Games/types/Mafia/effects/Scrambled.js
+++ Games/types/Mafia/effects/Scrambled.js
@@ -5,6 +5,7 @@ module.exports = class Scrambled extends Effect {
   constructor(lifespan) {
     super("Scrambled");
     this.lifespan = lifespan || Infinity;
+     this.isHarmful = true;
   }
 
   hear(message) {
