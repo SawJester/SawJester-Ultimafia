87c14c854c76b50137763f60ab4af9c011e4c8ed
SawJester <anglelord270@gmail.com>
2025-10-09 11:25:52 -0400

    Update SafeFromEffects.js

--- Games/types/Mafia/effects/SafeFromEffects.js
+++ Games/types/Mafia/effects/SafeFromEffects.js
@@ -1,9 +1,10 @@
 const Effect = require("../Effect");
 
 module.exports = class SafeFromEffects extends Effect {
-  constructor(immunity, lifespan) {
+  constructor(protector, immunity, lifespan) {
     super("SafeFromEffects");
 
+    this.protector = protector;
     this.immunity["poison"] = immunity || 5;
     this.immunity["malicious effect"] = immunity || 5;
     this.immunity["delirium"] = immunity || 5;
