188d94edfa0856f695e6f868b11e18dce66673fc
SawJester <SawJester@users.noreply.github.com>
2025-10-09 16:22:39 +0000

    Prettified Code!

--- Games/types/Mafia/Action.js
+++ Games/types/Mafia/Action.js
@@ -28,10 +28,9 @@ module.exports = class MafiaAction extends Action {
     power = power || 1;
     target = target || this.target;
 
-    
     //target.setTempImmunity("poison", power);
     //target.setTempImmunity("delirium", power);
-    let effect = player.giveEffect("SafeFromEffects",this.actor,power,1,);
+    let effect = player.giveEffect("SafeFromEffects", this.actor, power, 1);
     effect.source = this.role;
     this.passiveEffects.push(effect);
 
